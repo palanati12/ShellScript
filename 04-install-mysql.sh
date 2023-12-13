3ab33893c87ce12c5fb93172b74366825aba9b6d
palanati12 <palanati12@github.com>
2023-12-13 16:52:52 +0530

    install script for mysql

--- 04-install-mysql.sh
+++ 04-install-mysql.sh
@@ -5,7 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
 echo "ERROR:please run te script with root access"
+exit 1
 else
 echo "you are the root acess"
 fi
+install mysql -y
 
+if [$? -ne 0]
+then
+    echo "ERROR::Installing mysql is failed"
+    exit 1
+else
+    echo "mysql is sucessfully installed"
+fi
