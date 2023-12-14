49fdf6836bc7b77d77bb687e72c2ab83553f88e7
palanati12 <palanati12@github.com>
2023-12-14 14:11:02 +0530

    practing installing packages

--- 07-install-packages.sh
+++ 07-install-packages.sh
@@ -13,29 +13,29 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-    echo -u "ERROR:: $2 ....$R is failed $N"
+    echo -e "ERROR:: $2 ....$R is failed $N"
 else
-    echo -u "$2 ....$G is success $N" 
+    echo -e "$2 ....$G is success $N" 
 fi           
 }
 
 if [ $ID -ne 0 ]
 then
-    echo -u "$R ERROR::please run the script with root access $N"
+    echo -e "$R ERROR::please run the script with root access $N"
     exit 1
 else
-    echo -u "$G You are the root user $N"  
+    echo -e "$G You are the root user $N"  
 fi
 
 for package in $@
 do
-  yum list installed $package
-  if ($? -ne o)
+  yum list installed $package  #to konw the list of packages installed here
+  if ($? -ne 0 )
   then
       yum install $package -y &>> $LOGFILE
       VALIDATE $?  "installation of $package"
   else 
-      echo -u "$package is already installed...$Y skipping $N "
+      echo -e "$package is already installed...$Y skipping $N "
 fi
 done
       
