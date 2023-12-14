ca94717a996360cb1d8b98ef6044fc2ae18a1af3
palanati12 <palanati12@github.com>
2023-12-14 10:24:06 +0530

    practice for installa and logfiles

---
+++ 04.Functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ID=$( id -u ) 
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+if [ $1 -ne o ]   
+then
+    echo "installing $2 is failed"
+else 
+    echo " installing $2 is success"  
+fi          
+}
+
+
+if [ ID -ne 0 ]
+then 
+    echo "please access with root user"
+    exit 1
+else 
+    echo "you are a root user"
+
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $?  "mysql"
+
+yum install git -y &>>$LOGFILE
+
+VALIDATE $? "git"
+
+
+
