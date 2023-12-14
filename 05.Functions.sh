1bed5872cff654e7a0415c58fb4ccac018ea577b
palanati12 <palanati12@github.com>
2023-12-14 11:14:44 +0530

    practing mysql installation

--- 05.Functions.sh
+++ 05.Functions.sh
@@ -11,12 +11,13 @@ echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m
+N="\e[0m"
 
 VALIDATE(){
 if [ $1 -ne 0 ]   
 then
     echo -e "installing $2 $R is failed $N"
+exit 1    
 else 
     echo -e "installing $2 is $G success $N"  
 fi          
