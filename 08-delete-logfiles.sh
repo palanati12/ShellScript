2c624f07366340fe5fec4a143a4d2b87287e1d94
palanati12 <palanati12@github.com>
2023-12-18 14:02:18 +0530

    add code for delt the log files

--- 08-delete-logfiles.sh
+++ 08-delete-logfiles.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 SOURCE_DIR="/tmp/shellscript.logs/"
 
-if [! SOURCE_DIR ]
+if [ ! -d $SOURCE_DIR ]
 then
     echo -e " $R source directory: $SOURCE_DIR does not exists $N"
 fi
@@ -12,4 +17,5 @@ FILES_TO_DEL=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 while IFS = read -r line 
 do 
   echo "deleting file:$line"
+  rm -rf $line
 done  <<< $FILES_TO_DEL
