5726797b7c1bba7d163fa00bbc8be69ebca95eb6
palanati12 <palanati12@github.com>
2023-12-19 10:28:49 +0530

    reading files  for etc/passwds

--- 09-ifs.sh
+++ 09-ifs.sh
@@ -14,7 +14,7 @@ fi
 
 while IFS= ":" read -r username password user_id group_id user_fullname home_dir shell_path
 do 
-   echo "username:$username"
-   echo "user_id:$user_id"
-   echo "user_fullname:$user_fullname"
+   echo "username: $username"
+   echo "user_id: $user_id"
+   echo "user_fullname: $user_fullname"
 done << $FILE
