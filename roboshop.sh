352e5d28245656b0c28bf411f80e98e7c1c81038
palanati12 <palanati12@github.com>
2023-12-25 11:05:16 +0530

    code for creating ec2 instance for automation

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-0c440481abe2aae49 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-#ZONE_ID=Z104317737D96UJVA7NEF
-#DOMAIN_NAME="daws76s.online"
+ZONE_ID=Z065152639OW9A5BH7SLY
+DOMAIN_NAME="praveenapp.online"
 
 for i in "${INSTANCES[@]}"
 do
@@ -17,4 +17,26 @@ do
 
     IP_ADRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c440481abe2aae49 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+done
+
 
